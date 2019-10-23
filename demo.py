4fd69e9ab3d27b49a29125d223ae1856268c5fee
guoruibiao <marksinoberg@gmail.com>
2019-10-23 10:44:22 +0800

    测试下imageio生成gif的流程

--- demo.py
+++ demo.py
@@ -3,6 +3,17 @@
 """
 demo for resize images with configuration.
 """
+import imageio
+
+def create_gif(savename, duration=1):
+    frames = []
+    frames.append(imageio.imread("test.jpg"))
+    frames.append(imageio.imread("test1.jpg"))
+    frames.append(imageio.imread("test2.jpg"))
+    # duration 是没幅图设置停留的时间
+    imageio.mimsave(savename, frames, "GIF", duration=duration)
+
 
 if __name__ == "__main__":
-    print("demo.")
+    create_gif("test.gif")
+    print("done.")
