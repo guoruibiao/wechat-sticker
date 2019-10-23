9c609a309a752c0fbc853fbbe76de69a6ddf4860
guoruibiao <marksinoberg@gmail.com>
2019-10-23 15:23:48 +0800

    用代码生成一批图片并进行gif制作

--- demo.py
+++ demo.py
@@ -4,14 +4,20 @@
 demo for resize images with configuration.
 """
 import os, imageio
+from random import randint
 from PIL import Image, ImageFont, ImageDraw
 
-def create_gif(savename, duration=1):
+def create_gif(imgpath, savename, duration=1):
     frames = []
-    frames.append(imageio.imread("test.jpg"))
-    frames.append(imageio.imread("test1.jpg"))
-    frames.append(imageio.imread("test2.jpg"))
-    # duration 是每幅图设置停留的时间
+    if imgpath.endswith("/"):
+        imgpath = imgpath.rstrip("/")
+    for index in range(24):
+        fullname = "{}{}{}.png".format(imgpath, os.path.sep, index)
+        frames.append(imageio.imread(fullname))
+    # frames.append(imageio.imread("test.jpg"))
+    # frames.append(imageio.imread("test1.jpg"))
+    # frames.append(imageio.imread("test2.jpg"))
+    # # duration 是每幅图设置停留的时间
     imageio.mimsave(savename, frames, "GIF", duration=duration)
 
 
@@ -23,18 +29,28 @@ def resize_image(sourcepath, savepath, width, height):
     resizedImg = img.resize((width, height), Image.ANTIALIAS)
     resizedImg.save(savepath)
 
-def create_text_png(text, size=(240, 240), colors=(255, 255, 255)):
+def create_text_png(text, size=(240, 240), colors=(255, 255, 255), savefolder="/tmp"):
     img = Image.new("RGBA", size, colors)
     drawboard = ImageDraw.Draw(img)
     fontsize = size[0] // 2
     font = ImageFont.truetype("NewYork.ttf", fontsize)
-    x, y =(size[0] - fontsize), (size[1] - fontsize) // 2
+    x, y = (size[0] - fontsize), (size[1] - fontsize) // 2
     drawboard.text((x, y), text, font=font, fill="#000000")
     # img.show()
-    img.save("./created_text.png")
+    if savefolder.endswith("/"):
+        savefolder = savefolder.rstrip("/")
+    # folderpath / filename .png
+    img.save("{}{}{}.png".format(savefolder, os.path.sep, text))
+
+def one_stop_service():
+    # 随机生成 24张背景色随机的图片
+    folderpath = "/tmp"
+    for index in range(24):
+        colors = (randint(0, 255), randint(0, 255), randint(0, 255))
+        create_text_png(str(index), colors=colors, savefolder=folderpath)
+    # 根据套图生成一张gif看看效果
+    create_gif(folderpath, "numbers.gif", duration=0.3)
+
 
 if __name__ == "__main__":
-    # create_gif("test.gif")
-    # print("done.")
-    # resize_image("/tmp/test.jpg", "/tmp/test.resized.jpg", 240, 240)
-    create_text_png("1")
+    one_stop_service()
