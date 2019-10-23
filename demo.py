2f7de1d3b94afb6bd3c7e686dfe935dba19059ee
guoruibiao <marksinoberg@gmail.com>
2019-10-23 15:02:35 +0800

    添加根据文字生成图片的demo

--- demo.py
+++ demo.py
@@ -3,8 +3,8 @@
 """
 demo for resize images with configuration.
 """
-import imageio
-from PIL import Image
+import os, imageio
+from PIL import Image, ImageFont, ImageDraw
 
 def create_gif(savename, duration=1):
     frames = []
@@ -23,9 +23,18 @@ def resize_image(sourcepath, savepath, width, height):
     resizedImg = img.resize((width, height), Image.ANTIALIAS)
     resizedImg.save(savepath)
 
-
+def create_text_png(text, size=(240, 240), colors=(255, 255, 255)):
+    img = Image.new("RGBA", size, colors)
+    drawboard = ImageDraw.Draw(img)
+    fontsize = size[0] // 2
+    font = ImageFont.truetype("NewYork.ttf", fontsize)
+    x, y =(size[0] - fontsize), (size[1] - fontsize) // 2
+    drawboard.text((x, y), text, font=font, fill="#000000")
+    # img.show()
+    img.save("./created_text.png")
 
 if __name__ == "__main__":
     # create_gif("test.gif")
     # print("done.")
-    resize_image("/tmp/test.jpg", "/tmp/test.resized.jpg", 240, 240)
+    # resize_image("/tmp/test.jpg", "/tmp/test.resized.jpg", 240, 240)
+    create_text_png("1")
